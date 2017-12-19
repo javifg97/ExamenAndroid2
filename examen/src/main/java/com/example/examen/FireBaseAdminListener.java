a6d15330099d1a35c319456046714f18558a97a6
Javier Fernández García <javier.fernandez3@live.u-tad.com>
2017-12-19 12:20:18 +0100

    Creadas las Clases: SegundaActivity,FirebaseAdmin(Con su interfaz listener correspondiente) y DataHolder

---
+++ examen/src/main/java/com/example/examen/FireBaseAdminListener.java
@@ -0,0 +1,8 @@
+package com.example.examen;
+
+/**
+ * Created by javier.fernandez3 on 19/12/2017.
+ */
+
+public interface FireBaseAdminListener {
+}
---
+++ examen/src/main/java/com/example/examen/secondActivity.java
@@ -0,0 +1,9 @@
+package com.example.examen;
+
+/**
+ * Created by javier.fernandez3 on 19/12/2017.
+ */
+
+public class secondActivity {
+
+}
