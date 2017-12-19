220d1d91ac9a4866b23b67f9f09bca9cdd5c15a9
Javier Fernández García <javier.fernandez3@live.u-tad.com>
2017-12-19 14:04:09 +0100

    Rellenadas clases: >NoticiaViewHolder: Creado el molde para las celdas Añadido la libreria de RecycleView al geadle

--- examen/src/main/java/com/example/examen/Adapters/ListaNoticiasAdapter.java
+++ examen/src/main/java/com/example/examen/Adapters/ListaNoticiasAdapter.java
@@ -1,13 +1,29 @@
 package com.example.examen.Adapters;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by javier.fernandez3 on 19/12/2017.
  */
 
 public class ListaNoticiasAdapter {
+
 }
 
 //Este va a ser el molde para cada celda
-class NoticiaViewHolder{
+class NoticiaViewHolder  extends RecyclerView.ViewHolder{
+    public TextView tvNombre;
+    public ImageView ivImagen;
+    public NoticiaViewHolder(View view){
+        super(itemView);
+        this.tvNombre=view.itemView.g
+
+    }
+
+
+
 
 }
