901dfa32cf0cf668820fcc0f2739e2a89d230e5f
Javier Fernández García <javier.fernandez3@live.u-tad.com>
2017-12-19 13:23:26 +0100

    Rellenadas clases: >secondActivity: Creado el fragment >activity_second: añadido el fragment en el xml

--- examen/src/main/java/com/example/examen/secondActivity.java
+++ examen/src/main/java/com/example/examen/secondActivity.java
@@ -3,11 +3,39 @@ package com.example.examen;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.milib.ListaFragment;
+
 public class secondActivity extends AppCompatActivity {
 
+        ListaFragment listaFragmennoticias;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        secondActivityEvents events =new secondActivityEvents(this);
+
+        listaFragmennoticias =(ListaFragment)getSupportFragmentManager().findFragmentById(R.id.frgLista);
+
+        DataHolder.instance.fireBaseAdmin.setListener(events);
+        
+
+    }
+}
+class secondActivityEvents implements FireBaseAdminListener{
+    secondActivity secondActivity;
+
+    public secondActivityEvents(secondActivity secondActivity){
+        this.secondActivity = secondActivity;
+    }
+
+    @Override
+    public void firebaseAdmin_registerOk(boolean blOk) {
+
+    }
+
+    @Override
+    public void firebaseAdmin_loginOk(boolean blOk) {
+
     }
 }
