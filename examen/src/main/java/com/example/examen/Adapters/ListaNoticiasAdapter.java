6534939b29d3e79dfe11b8243d5ad102edb5f154
Javier Fernández García <javier.fernandez3@live.u-tad.com>
2017-12-19 13:54:02 +0100

    Creado: >Mi paquete de Adapters >Adapter de Noticias Rellenadas clases: >ListaNoticiasAdapter: rellenada para crear la clase NoticiaViewHolder

---
+++ examen/src/main/java/com/example/examen/Adapters/ListaNoticiasAdapter.java
@@ -0,0 +1,13 @@
+package com.example.examen.Adapters;
+
+/**
+ * Created by javier.fernandez3 on 19/12/2017.
+ */
+
+public class ListaNoticiasAdapter {
+}
+
+//Este va a ser el molde para cada celda
+class NoticiaViewHolder{
+
+}
