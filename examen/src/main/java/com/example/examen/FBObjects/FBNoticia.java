6a8ca50084e962203e90a583eb0ef29f90f7334c
Javier Fernández García <javier.fernandez3@live.u-tad.com>
2017-12-19 13:39:40 +0100

    Creado: >Mi paquete de Objetos de firebase >Clase FBNoticia Rellenadas clases: >FBNoticia: rellenada a medida de Firebase

---
+++ examen/src/main/java/com/example/examen/FBObjects/FBNoticia.java
@@ -0,0 +1,19 @@
+package com.example.examen.FBObjects;
+
+/**
+ * Created by javier.fernandez3 on 19/12/2017.
+ */
+
+public class FBNoticia {
+    public String Noticia;
+    public String notUrl;
+
+    public FBNoticia(){
+
+    }
+
+    public FBNoticia(String Noticia,String notUrl){
+        this.Noticia = Noticia;
+        this.notUrl= notUrl;
+    }
+}
