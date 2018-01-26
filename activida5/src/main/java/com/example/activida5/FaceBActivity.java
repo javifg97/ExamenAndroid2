4b903f060cba6eaf905ff9701aa06ecd1cdc00d6
Javier Fernández García <javier.fernandez3@live.u-tad.com>
2018-01-26 12:54:41 +0100

    FB 2 video

---
+++ activida5/src/main/java/com/example/activida5/FaceBActivity.java
@@ -0,0 +1,13 @@
+package com.example.activida5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FaceBActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face_b);
+    }
+}
